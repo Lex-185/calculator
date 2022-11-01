899efec0588758518c88124f5b7d2928ee4f24f6
Lex <gravityandmadness@gmail.com>
2022-11-01 13:37:01 +0200

    Global vars and dom elements selected

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+// Vars
+const newNumber = document.querySelector('.newNumber');
+const storedNumber = document.querySelector('.storedNumber');
+
+const equals = document.getElementById('equals');
+const decimal = document.getElementById('decimal');
+const allClear = document.getElementById('clear');
+
+const calcDigits = document.querySelectorAll('.button');
+const calcOperator = document.querySelectorAll('.operator')
+
 // Basic math operators
 const add = (x,y) => {return x + y};
 const sub = (x,y) => {return x - y};
