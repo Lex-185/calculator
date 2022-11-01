9e3f8cc64ccab1c6ddbf4e95922b2b452e51a679
Lex <gravityandmadness@gmail.com>
2022-11-01 10:38:24 +0200

    Add basic math operators

---
+++ main.js
@@ -0,0 +1,6 @@
+// Basic math operators
+const add = (x,y) => {return x + y};
+const sub = (x,y) => {return x - y};
+const multiply = (x,y) => {return x * y};
+const divide = (x,y) => {return x / y};
+const remainder = (x,y) => {return x % y};
