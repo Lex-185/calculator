2b87b9b997bb7bfa6d84df3c592309623a26ab98
Lex <gravityandmadness@gmail.com>
2022-11-01 10:40:51 +0200

    Create function that calls math operator on 2 numbers

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ const sub = (x,y) => {return x - y};
 const multiply = (x,y) => {return x * y};
 const divide = (x,y) => {return x / y};
 const remainder = (x,y) => {return x % y};
+
+// Operate function: Takes an operator and 2 numbers. Calls function on the 2 numbers
+function operate(operator, x, y) {
+    if (operator === '+') {
+        return add(x,y);
+    } else if (operator === '-') {
+        return sub(x,y);
+    } else if (operator === '*') {
+        return multiply(x,y);
+    } else if (operator === '/') {
+        return divide(x,y);
+    } else if (operator === '%') {
+        return remainder(x,y);
+    }
+}
