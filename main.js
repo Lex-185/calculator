f4148b88bc9a8535c4bf28661a65750e9cc272a9
Lex <gravityandmadness@gmail.com>
2022-11-02 11:37:40 +0200

    Operate function displays results in output section

--- main.js
+++ main.js
@@ -1,15 +1,47 @@
 // Vars
-const newNumber = document.querySelector('.newNumber');
-const storedNumber = document.querySelector('.storedNumber');
+const calcInputDisplay = document.querySelector('.calcInputDisplay');
+const calcOutputDisplay = document.querySelector('.calcOutputDisplay');
 
-const equals = document.getElementById('equals');
+
 const decimal = document.getElementById('decimal');
-const allClear = document.getElementById('clear');
 
-const calcDigits = document.querySelectorAll('.button');
-const calcOperator = document.querySelectorAll('.operator')
+const calcButtons = document.querySelectorAll('.button');
+const calcOperators = document.querySelectorAll('.operator')
+let placeholder = '';
+let storedValue = ''
+
+calcButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        buttonValue(e.target.textContent);
+    })
+})
+
+calcOperators.forEach(operator => {
+    operator.addEventListener('click', (e) => {
+        operatorValue(e.target.textContent);
+    })
+})
+
+const buttonValue = (number) => {
+    if (placeholder.length <= 12) {
+        placeholder += number
+        calcInputDisplay.textContent = placeholder
+    }
+}
+
+const operatorValue = (mathKey) => {
+    operator = mathKey;
+    storedValue = placeholder;
+    calcOutputDisplay.textContent = `${storedValue} ${operator}`
+    placeholder = '';
+}
+
+// Equals
+const equals = document.getElementById('equals');
+equals.addEventListener('click', operate)
 
 // Basic math operators
+let operator = ''
 const add = (x,y) => {return x + y};
 const sub = (x,y) => {return x - y};
 const multiply = (x,y) => {return x * y};
@@ -17,16 +49,25 @@ const divide = (x,y) => {return x / y};
 const remainder = (x,y) => {return x % y};
 
 // Operate function: Takes an operator and 2 numbers. Calls function on the 2 numbers
-function operate(operator, x, y) {
+function operate() {
+    let x = parseInt(storedValue)
+    let y = parseInt(placeholder)
+
     if (operator === '+') {
-        return add(x,y);
+        console.log(x, y);
+        calcInputDisplay.textContent = add(x, y);
     } else if (operator === '-') {
-        return sub(x,y);
-    } else if (operator === '*') {
-        return multiply(x,y);
-    } else if (operator === '/') {
-        return divide(x,y);
+        calcInputDisplay.textContent = sub(x, y);
+    } else if (operator === 'x') {
+        calcInputDisplay.textContent = multiply(x, y);
+    } else if (operator === '÷') {
+        calcInputDisplay.textContent = divide(x, y);
     } else if (operator === '%') {
-        return remainder(x,y);
+        calcInputDisplay.textContent = remainder(x, y);
     }
+    calcOutputDisplay.textContent = ''
+    
 }
+
+// All Clear
+const allClear = document.getElementById('clear');
